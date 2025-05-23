a20ff2c8bbb12e8ddceb123582a7e7b60eb3f2c3
Kelvin Mwangi <kelvin@nathandigital.com>
2025-05-23 17:07:06 +0300

    set up changes

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String [] arr = {"name", "other"};
+    }
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
